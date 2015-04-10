d92b45e8ee0096342e15f74c43a7047d1910badb
gargan26 <gargan26@users.noreply.github.com>
2015-04-10 07:44:27 -0400

    still not done with the NBT code

--- nbt.py
+++ nbt.py
@@ -18,6 +18,7 @@ import logging
 import gzip
 import collections
 import struct
+import numpy
 
 log = logging.getLogger(__name__)
 
@@ -39,42 +40,140 @@ string_len_format = struct.Struct('>H')
 
 
 class TAG_Value(object):
-    pass
+    '''Simple values. Subclasses override fmt to change the type and size. Subclasses may set data_type instead of
+    overriding setValue for automatic data type coercion'''
+    __slots__ = ('_name', '_value')
+
+    def __init__(self, value=0, name=''):
+        self.value = value
+        self.name = name
+
+    fmt = struct.Struct('=c')
+    tagID = NotImplemented
+    data_type = NotImplemented
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        '''Change the TAG's value. Data types are checked and coerced if needed.'''
+        self._value = self.data_type(new_value)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        '''Change the TAG's name. Coerced to a unicode.'''
+        self._name = str(new_name)
+
+    @classmethod
+    def load_from(cls, ctx):
+        data = ctx.data[ctx.offset:]
+        value, = cls.fmt.unpack_from(data)
+        self = cls(value=value)
+        ctx.offset += self.fmt.size
+        return self
+
+    # def __repr__(self):
+    #     return '<%s name="%s" value=%r>' % (str(self.__class__.__name__), self.name, self.value)
+
+    # def write_tag(self, buf):
+    #     buf.write(chr(self.tagID))
+
+    # def write_name(self, buf):
+    #     if self.name is not None:
+    #         write_string(self.name, buf)
+
+    # def write_value(self, buf):
+    #     buf.write(self.fmt.pack(self.value))
 
 
-class TAG_Byte(object):
+class TAG_Byte(TAG_Value):
+    __slots__ = ()
     tagID = TAG_BYTE
+    fmt = struct.Struct('>b')
+    data_type = int
 
 
-class TAG_Short(object):
+class TAG_Short(TAG_Value):
+    __slots__ = ()
     tagID = TAG_SHORT
+    fmt = struct.Struct('>h')
+    data_type = int
 
 
-class TAG_Int(object):
+class TAG_Int(TAG_Value):
+    __slots__ = ()
     tagID = TAG_INT
+    fmt = struct.Struct('>i')
+    data_type = int
 
 
-class TAG_Long(object):
+class TAG_Long(TAG_Value):
+    __slots__ = ()
     tagID = TAG_LONG
+    fmt = struct.Struct('>q')
+    data_type = int
 
 
-class TAG_Float(object):
+class TAG_Float(TAG_Value):
+    __slots__ = ()
     tagID = TAG_FLOAT
+    fmt = struct.Struct('>f')
+    data_type = float
 
 
-class TAG_Double(object):
+class TAG_Double(TAG_Value):
+    __slots__ = ()
     tagID = TAG_DOUBLE
+    fmt = struct.Struct('>d')
+    data_type = float
 
 
-class TAG_Byte_Array(object):
+class TAG_Byte_Array(TAG_Value):
+    '''Like a string, but for binary data. Four length bytes instead of two. Value is a numpy array, and you can change
+    its elements'''
+    __slots__ = ()
+
     tagID = TAG_BYTE_ARRAY
 
+    def __init__(self, value=None, name=''):
+        if value is None:
+            value = numpy.zeros(0, self.dtype)
+        self.name = name
+        self.value = value
 
-class TAG_String(object):
+    def __repr__(self):
+        return '<{0} name="{1}" length={2}>'.format(self.__class__, self.name, len(self.value))
+
+    def data_type(self, value):
+        return numpy.array(value, self.dtype)
+
+    dtype = numpy.dtype('uint8')
+
+    @classmethod
+    def load_from(cls, ctx):
+        data = ctx.data[ctx.offset:]
+        string_len, = TAG_Int.fmt.unpack_from(data)
+        value = fromstring(data[4:string_len * cls.dtype.itemsize + 4], cls.dtype)
+        self = cls(value)
+        ctx.offset += string_len * cls.dtype.itemsize + 4
+        return self
+
+    # def write_value(self, buf):
+    #     value_str = self.value.tostring()
+    #     buf.write(struct.pack('>I%ds' % (len(value_str),), self.value.size, value_str))
+
+
+class TAG_String(TAG_Value):
     tagID = TAG_STRING
 
 
-class TAG_List(object):
+class TAG_List(TAG_Value, collections.MutableSequence):
     tagID = TAG_LIST
 
 
@@ -94,16 +193,16 @@ class TAG_Compound(TAG_Value, collections.MutableMapping):
     def __repr__(self):
         return '<{0} name="{1}" keys={2!r}>'.format(self.__class__.__name__, self.name, list(self.keys()))
 
-    # def data_type(self, val):
-    #     for i in val:
-    #         self.check_value(i)
-    #     return list(val)
-    #
-    # def check_value(self, val):
-    #     if not isinstance(val, TAG_Value):
-    #         raise TypeError('Invalid type for TAG_Compound element: %s' % val.__class__.__name__)
-    #     if not val.name:
-    #         raise ValueError('Tag needs a name to be inserted into TAG_Compound: %s' % val)
+    def data_type(self, val):
+        for i in val:
+            self.check_value(i)
+        return list(val)
+
+    def check_value(self, val):
+        if not isinstance(val, TAG_Value):
+            raise TypeError('Invalid type for TAG_Compound element: {0}'.format(val.__class__.__name__))
+        if not val.name:
+            raise ValueError('Tag needs a name to be inserted into TAG_Compound: {0}'.format(val))
 
     @classmethod
     def load_from(cls, ctx):
@@ -153,7 +252,7 @@ class TAG_Compound(TAG_Value, collections.MutableMapping):
     #         f.write(data)
     #     else:
     #         filename_or_buf.write(data)
-    #
+
     # def write_value(self, buf):
     #     for tag in self.value:
     #         tag.write_tag(buf)
@@ -161,57 +260,54 @@ class TAG_Compound(TAG_Value, collections.MutableMapping):
     #         tag.write_value(buf)
     #
     #     buf.write('\x00')
-    #
-    # # --- collection functions ---
-    #
-    # def __getitem__(self, key):
-    #     # hits=filter(lambda x: x.name==key, self.value)
-    #     # if(len(hits)): return hits[0]
-    #     for tag in self.value:
-    #         if tag.name == key:
-    #             return tag
-    #     raise KeyError('Key {0} not found'.format(key))
-    #
-    # def __iter__(self):
-    #     return map(lambda x: x.name, self.value)
-    #
+
+    # --- collection functions ---
+
+    def __delitem__(self, key):
+        self.value.__delitem__(self.value.index(self[key]))
+
+    def __getitem__(self, key):
+        for tag in self.value:
+            if tag.name == key:
+                return tag
+        raise KeyError('Key {0} not found'.format(key))
+
+    def __iter__(self):
+        return map(lambda x: x.name, self.value)
+
+    def __len__(self):
+        return self.value.__len__()
+
+    def __setitem__(self, key, item):
+        '''Automatically wraps lists and tuples in a TAG_List, and wraps strings in a TAG_String.'''
+        if isinstance(item, (list, tuple)):
+            item = TAG_List(item)
+        elif isinstance(item, str):
+            item = TAG_String(item)
+
+        item.name = key
+        self.check_value(item)
+
+        # remove any items already named 'key'.
+        # if not self.ALLOW_DUPLICATE_KEYS:
+        self._value = [x for x in self._value if x.name != key]
+
+        self._value.append(item)
+
     # def __contains__(self, key):
     #     return key in [x.name for x in self.value]
-    #
-    # def __len__(self):
-    #     return self.value.__len__()
-    #
-    # def __setitem__(self, key, item):
-    #     '''Automatically wraps lists and tuples in a TAG_List, and wraps strings
-    #     and unicodes in a TAG_String.'''
-    #     if isinstance(item, (list, tuple)):
-    #         item = TAG_List(item)
-    #     elif isinstance(item, str):
-    #         item = TAG_String(item)
-    #
-    #     item.name = key
-    #     self.check_value(item)
-    #
-    #     # remove any items already named 'key'.
-    #     if not self.ALLOW_DUPLICATE_KEYS:
-    #         self._value = [x for x in self._value if x.name != key]
-    #
-    #     self._value.append(item)
-    #
-    # def __delitem__(self, key):
-    #     self.value.__delitem__(self.value.index(self[key]))
-    #
+
     # def add(self, value):
     #     if value.name is None:
     #         raise ValueError('Tag %r must have a name.' % value)
     #
     #     self[value.name] = value
-    #
+
     # def get_all(self, key):
     #     return [v for v in self._value if v.name == key]
 
 
-class TAG_Int_Array(object):
+class TAG_Int_Array(TAG_Byte_Array):
     tagID = TAG_INT_ARRAY
 
 tag_classes = {cls.tagID: cls for cls in (TAG_Byte, TAG_Short, TAG_Int, TAG_Long, TAG_Float, TAG_Double, TAG_Byte_Array, TAG_String, TAG_List, TAG_Compound, TAG_Int_Array)}
